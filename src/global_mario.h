ae50e5c14195a08ece039cea459fa7f3df2a3fc1
tkk <tkk@hongo.wide.ad.jp>
2015-05-16 05:35:00 -0400

    add declarations of variables.

--- src/global_mario.h
+++ src/global_mario.h
@@ -6,14 +6,28 @@
 #ifndef ROUTEMARIO_H
 #define ROUTEMARIO_H
 
-/* eth.h */
+#include "interfaces.h"
+#include "mbuf_queue.h"
+#include "eth.h"
+#include "ipv4.h"
+#include "arp.h"
 
+/* eth.h */
+RTE_DECLARE_PER_LCORE(uint16_t, nic_queue_id);
+RTE_DECLARE_PER_LCORE(struct mbuf_queue **, eth_tx_queue);
+extern struct fdb_table *fdb_tb;
 
 /* ipv4.h */
-
+RTE_DECLARE_PER_LCORE(struct mbuf_queue *, routing_queue);
 
 /* interfaces.h */
+extern struct l3_interfaces *intfs;
+
+/* arp.h */
+extern struct arp_table *arp_tb;
 
 
+/* fib.h */
+// extern ;
 
 #endif

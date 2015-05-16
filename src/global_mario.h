84d8dd75e50738d29d8c807c1740d42140548c1d
tkk <tkk@hongo.wide.ad.jp>
2015-05-16 08:30:22 -0400

    add getter function of global variables.

--- src/global_mario.h
+++ src/global_mario.h
@@ -15,10 +15,35 @@
 /* eth.h */
 RTE_DECLARE_PER_LCORE(uint16_t, nic_queue_id);
 RTE_DECLARE_PER_LCORE(struct mbuf_queue **, eth_tx_queue);
+static inline struct mbuf_queue**
+get_eth_tx_Qs() {
+  return RTE_PER_LCORE(eth_tx_queue);
+}
+
+static inline struct mbuf_queue*
+get_eth_tx_Q(uint8_t port_id) {
+  return (RTE_PER_LCORE(eth_tx_queue))[port_id];
+}
+
+static inline uint16_t
+get_nic_queue_id() {
+  return RTE_PER_LCORE(nic_queue_id);
+}
+
+static inline void
+set_nic_queue_id(uint16_t queue_id) {
+  RTE_PER_LCORE(nic_queue_id) = queue_id;
+}
+
 extern struct fdb_table *fdb_tb;
 
 /* ipv4.h */
 RTE_DECLARE_PER_LCORE(struct mbuf_queue *, routing_queue);
+static inline struct mbuf_queue*
+get_routing_Q() {
+  return RTE_PER_LCORE(routing_queue);
+}
+
 
 /* interfaces.h */
 extern struct l3_interfaces *intfs;

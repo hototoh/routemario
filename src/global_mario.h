55a5f65e1815cf5e8fd40ff9ec1623b80f02a9a7
tkk <tkk@hongo.wide.ad.jp>
2015-05-16 03:57:10 -0400

    rename routemario to global_mario

---
+++ src/global_mario.h
@@ -0,0 +1,19 @@
+/**
+ * global_mario.h: 
+ * declare global RouteMario's variables.
+ */
+
+#ifndef ROUTEMARIO_H
+#define ROUTEMARIO_H
+
+/* eth.h */
+
+
+/* ipv4.h */
+
+
+/* interfaces.h */
+
+
+
+#endif
